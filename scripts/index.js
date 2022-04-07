09d0226e0193005fe0e6e8b61747698606dd3bcd
igor-yakovlev <47921804+igor-yakovlev@users.noreply.github.com>
2022-04-07 19:53:27 +0300

    features/add card from data

--- scripts/index.js
+++ scripts/index.js
@@ -16,7 +16,10 @@ const nameInput = document.querySelector('#name');
 const descriptionInput = document.querySelector('#description');
 //  Основная страница
 const mainPage = document.querySelector('.page');
-
+//  Шаблон карточки
+const cardTemplate = document.querySelector('#card').content;
+//Контейнер с карточками
+const cardContainer = document.querySelector('.elements__items');
 
 
 // Функция открытия попапа
@@ -51,4 +54,85 @@ closeButton.addEventListener('click', closePopap);
 
 // Событие отправки данных формы
 form.addEventListener('submit', sendFormData);
+
+
+
+
+// Данные для карточек
+const initialCards = [
+  {
+    name: 'Карачаевск',
+    link: './images/elements/__item/karachaevsk.jpg'
+
+  },
+  {
+    name: 'Гора Эльбрус',
+    link: './images/elements/__item/gora_elbrus.png'
+
+  },
+  {
+    name: 'Домбай',
+    link: './images/elements/__item/dombai.jpg'
+
+  },
+  {
+    name: 'Адыл-Су',
+    link: 'https://risk.ru/u/img/319/318712.jpeg'
+
+  },
+  {
+    name: 'Кавказские горы',
+    link: './images/elements/__item/gori_kavkaz.jpg'
+
+  },
+  {
+    name: 'Голубые озера в Кабардино-Балкарии',
+    link: './images/elements/__item/golubie_ozera.jpg'
+
+  },
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+
+// Функция добавления карточки
+function addCard (data) {
+  const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+  cardElement.querySelector('.card__image').src = data.link;
+  cardElement.querySelector('.card__title').textContent = data.name;
+  cardContainer.append(cardElement);
+
+}
+
+
+// Цикл, добавляющий карточки в соответствии с данными
+for (card of initialCards) {
+  addCard(card);
+}
+
+
+
+
+
 
