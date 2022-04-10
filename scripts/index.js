f966a3803bc520da0d01d99cf6d162cea33840f4
igor-yakovlev <47921804+igor-yakovlev@users.noreply.github.com>
2022-04-10 13:07:43 +0300

    hotfixes/second refactoring

--- scripts/index.js
+++ scripts/index.js
@@ -34,7 +34,8 @@ const linkInput = formAddPlace.querySelector('#link');
 const cardTemplate = document.querySelector('#card').content;
 //Контейнер с карточками
 const cardContainer = document.querySelector('.elements__items');
-
+//Картинка в попапе картинки
+const popapImage = popapImagePlace.querySelector('.popap__image');
 
 
 // Функция открытия попапа
@@ -47,52 +48,6 @@ function closePopap (popap) {
   popap.classList.remove('popap_opened');
 }
 
-
-
-
-
-// Данные для карточек
-const initialCards = [
-  {
-    name: 'Адыл-Су',
-    link: 'https://risk.ru/u/img/319/318712.jpeg'
-
-  },
-  {
-    name: 'Кавказские горы',
-    link: './images/elements/__item/gori_kavkaz.jpg'
-
-  },
-  {
-    name: 'Голубые озера в Кабардино-Балкарии',
-    link: './images/elements/__item/golubie_ozera.jpg'
-
-  },
-  {
-    name: 'Архыз',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
-  },
-  {
-    name: 'Челябинская область',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
-  },
-  {
-    name: 'Иваново',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
-  },
-  {
-    name: 'Камчатка',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
-  },
-  {
-    name: 'Холмогорский район',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
-  },
-  {
-    name: 'Байкал',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
-  }
-];
 
 // Функция создания карточки
 function createCard ({link, name}) {
@@ -115,14 +70,13 @@ function createCard ({link, name}) {
   const del = cardElement.querySelector('.card__delete');
   // Событие удаления карточки
   del.addEventListener('click', (e) => {
-    delCard = e.target.closest('.card');
+    const delCard = e.target.closest('.card');
     delCard.remove();
   });
 
   // Событие открытия попапа картинки
   image.addEventListener('click', (e) => {
     openPopap(popapImagePlace);
-    const popapImage = popapImagePlace.querySelector('.popap__image');
     popapImage.src = e.target.src;
     popapImage.alt = e.target.alt;
     const popapDescription = popapImagePlace.querySelector('.popap__description');
@@ -163,8 +117,6 @@ function renderCards () {
 renderCards();
 
 
-
-
 // Событие клика для открытия попапа данных пользователя
 editButton.addEventListener('click', () => {
   openPopap(popapUserInfo);
@@ -192,8 +144,6 @@ closeButtonAddPlace.addEventListener('click', () => {
 closeButtonImagePlace.addEventListener('click', () => {
   closePopap(popapImagePlace);
 });
-
-
 
 
 // Событие отправки формы данных пользователя
---
+++ scripts/initialCards.js
@@ -0,0 +1,42 @@
+const initialCards = [
+  {
+    name: 'Адыл-Су',
+    link: 'https://risk.ru/u/img/319/318712.jpeg'
+
+  },
+  {
+    name: 'Кавказские горы',
+    link: './images/elements/__item/gori_kavkaz.jpg'
+
+  },
+  {
+    name: 'Голубые озера в Кабардино-Балкарии',
+    link: './images/elements/__item/golubie_ozera.jpg'
+
+  },
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+
